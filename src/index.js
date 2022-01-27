8806588db0ccfdb1c0339bffe2a321dc3f9360f0
Melissa Bracamonte <melissabracamonte30@gmail.com>
2022-01-27 14:54:03 -0600

    Card validation final
    Versión final de la app que valida tarjeta de crédito

--- src/index.js
+++ src/index.js
@@ -1,54 +1,27 @@
 import validator from './validator.js';
-
-
-  //console.log(validator);
+//console.log(validator);
 
 // 1. Recibir el valor de la tarjeta de crédito del usuario.
 document.getElementById("boton").addEventListener("click", cardNumber);
 function cardNumber() {
-    let numero = document.getElementById("card1").value;
-    if(numero.length == 0){
-      numero = "m"
+  let numero = document.getElementById("card1").value;
+  if(numero.length == 0){
+    numero = "m"
+  }
+  //10. Mostrar mensaje en pantalla: tarjeta valida (verde) o tarjeta invalida (rojo), dependiendo del resultado del algoritmo de Luhn.
+  validator.isValid(numero);
+    let residuoX = validator.isValid(numero);
+    let textoH2 = document.getElementById("validationMessage");
+    if(residuoX === true){
+      textoH2.innerText = "Se ha registrado con éxito tu tarjeta";
+      document.getElementById("validationMessage1").style.background="green"; 
     }
-    validator.isValid(numero);
-};
-
-//10. Mostrar mensaje en pantalla: tarjeta valida o tarjeta invalida, dependiendo del resultado del algoritmo de Luhn.
-document.getElementById("boton").addEventListener("click", getResultStorage);
-function getResultStorage() {
-  let textoH2 = document.getElementById("validationMessage");
-  let resultStorage = localStorage.getItem("residuo1");
-  alert(resultStorage)
-  if(resultStorage == 0){
-    textoH2.innerText = "Tarjeta valida"; 
+    else {
+      textoH2.innerText = "Tarjeta invalida"; 
+      document.getElementById("validationMessage1").style.background="red";
+    }  
+  //11.2. Ocultar números de la tarjeta de crédito y dejar visibles solo los 4 últimos dígitos.
+  validator.maskify(numero);
+    let array2 = validator.maskify(numero);
+    document.getElementById("card1").value=array2;
 }
-  else {
-    textoH2.innerText = "Tarjeta invalida"; 
-  }
-  localStorage.clear();
-};
-
-//11. Ocultar números de la tarjeta de crédito y dejar visibles solo los 4 últimos dígitos.
-document.getElementById("boton").addEventListener("click", maskify1);
-function maskify1() {
-  let numeroTcUsuario = document.getElementById("card1");
-  if (numeroTcUsuario.type==="password"){
-   let arrayNumeroTcUsuario = numeroTcUsuario.split("");
-   //arrayNumeroTcUsuario.slice([-0 [, fin]])
-  }
-  else {
-      numeroTcUsuario.type="password"
-  }
-}
-
-
-//document.getElementById("mostrarNumero").addEventListener("click", showNumbers);
-//function showNumbers() {
-  //let numeroTcUsuario = document.getElementById("card1");
-  //if (numeroTcUsuario.type==="password"){
-      //numeroTcUsuario.type="text";
-  //}
-  //else {
-      //numeroTcUsuario.type="password"
-  //}
-//};
--- src/validator.js
+++ src/validator.js
@@ -1,45 +1,56 @@
 //2. Crear un objeto donde se desarrolla todo el algoritmo de Luhn.
 const validator = {
   isValid (numero){
-  //3. Convertir el valor del número de la tarjeta de crédito del usuario en un array.
+    //3. Convertir el valor del número de la tarjeta de crédito del usuario en un array.
     let array=numero.split("");
 
-  //4. Invertir el array
+    //4. Invertir el array
     let arrayInvertido=array.reverse();
 
-  //5. Multiplicar los números pares(que en el array corresponden a los impares) por 2 e incluir el resultado en el array.
+    //5. Multiplicar los números pares(que en el array corresponden a los impares) por 2 e incluir el resultado en el array.
     for(let i = 1; i < arrayInvertido.length; i=i+2){
       let numerosPares = arrayInvertido[i] * 2;
       arrayInvertido[i] = numerosPares
 
-  //6. Una vez multiplicados los pares, sumar los digitos del valor de los que sean igual o mayor a 10 (para esto hay que convertir los 
-  //números en un string, luego meterlos en un array, luego llamarlos desde sus posiciones en el array y finalmente hacer la suma).
-      if(numerosPares > 9){
-        let arrayDeNumerosPares =numerosPares.toString()
-        let digitos = arrayDeNumerosPares.split('');
-        let primerDigito = parseInt(digitos[0]);
-        let segundoDigito = parseInt(digitos[1]);
-        let sumaDosDigitos = primerDigito + segundoDigito;
-  //7. Una vez sumados los números del valor de los que sean igual o mayor a 10, incorporarlos al array
-        arrayInvertido[i] = sumaDosDigitos
-      }
+        //6. Una vez multiplicados los pares, sumar los digitos del valor de los que sean igual o mayor a 10 (para esto hay que convertir los 
+        //números en un string, luego meterlos en un array, luego llamarlos desde sus posiciones en el array y finalmente hacer la suma).
+        if(numerosPares > 9){
+          let arrayDeNumerosPares =numerosPares.toString()
+          let digitos = arrayDeNumerosPares.split('');
+          let primerDigito = parseInt(digitos[0]);
+          let segundoDigito = parseInt(digitos[1]);
+          let sumaDosDigitos = primerDigito + segundoDigito;
+          //7. Una vez sumados los números del valor de los que sean igual o mayor a 10, incorporarlos al array
+          arrayInvertido[i] = sumaDosDigitos
+        }
     }
 
-  //8. Suma de los valores impares y de los nuevos valores de los pares (que en el array corresponden a las posiciones impares)
-  let sumaFinal = 0;
-        for(let n = 0; n < arrayInvertido.length; n++){
-          arrayInvertido[n] = parseInt(arrayInvertido[n]);
-          sumaFinal += arrayInvertido[n];
-          console.log(sumaFinal)
-        }
+    //8. Suma de los valores impares y de los nuevos valores de los pares (que en el array corresponden a las posiciones impares)
+    let sumaFinal = 0;
+    for(let n = 0; n < arrayInvertido.length; n++){
+      arrayInvertido[n] = parseInt(arrayInvertido[n]);
+      sumaFinal += arrayInvertido[n];
+    }
 
-  //9. Sacar el residuo de una división del valor de la suma de los números impares y de los nuevos valores de los pares y guardar el 
-  //resultado en el local storage.
-        let residuo = sumaFinal % 10;
-        let saveResiduoInStorage = localStorage.setItem("residuo1", residuo);
-        console.log(arrayInvertido);
-        return residuo
-  }
+    //9. Sacar el residuo de una división del valor de la suma de los números impares y de los nuevos valores de los pares y guardar el 
+    //resultado en el local storage.
+    let residuo = sumaFinal % 10;
+      if(residuo === 0){
+        residuo = true;
+      }
+      else{
+        residuo = false;
+      }
+    return residuo;
+  },
+
+  //11.1. Ocultar números de la tarjeta de crédito y dejar visibles solo los 4 últimos dígitos.
+  maskify(numero){
+    let array2 = numero.split("");
+    for(let i = 0; i<array2.length - 4; i++){
+      array2[i]="#";
+    }
+        return array2.join("");
+    }
 };
-
 export default validator;
